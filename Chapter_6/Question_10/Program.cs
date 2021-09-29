2170f9bd0ee5555b20f7dae9a5dda3bd5c637b24
Mike Hall <Mike.Hall@vu.com>
2021-09-29 00:40:14 -0500

    Worked Problems

--- Chapter_6/Question_10/Program.cs
+++ Chapter_6/Question_10/Program.cs
@@ -6,7 +6,24 @@ namespace Question_10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter a number between 1 and 20: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 0 && n < 21)
+            {
+                for (int row = 1; row <= n; row++)
+                {
+                    for (int col = row; col < row + n; col++)
+                    {
+                        Console.Write(col + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("This number is out of the specified range.");
+            }
         }
     }
 }
