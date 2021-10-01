37ac6846e4a74875dee35d1bd0a19de2d450d465
Mike Hall <Mike.Hall@vu.com>
2021-10-01 00:55:04 -0500

    worked chapter 7

---
+++ Chapter_7/Question_10.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Question_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 4, 1, 1, 4, 2, 3, 4, 4, 4, 1, 2, 4, 9, 3 };
+            int count = 1;
+            int tempCount, currentNum;
+            int mode = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                currentNum = array[i];
+                tempCount = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (currentNum == array[j])
+                    {
+                        tempCount += 1;
+                    }
+                }
+                if (tempCount > count)
+                {
+                    mode = currentNum;
+                    count = tempCount;
+                }
+            }
+            Console.WriteLine("The mode is {0} ({1} times)", mode, count);
+       
+        }
+    }
+}
